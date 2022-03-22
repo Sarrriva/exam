eaa388761ea0c7ffc287edf6e43e82bf04ec65c9
Sarrriva <s.ivanova.kazanexpress@gmail.com>
2022-03-22 17:23:39 +0300

    Добавила параметриз.тест

--- src/test/java/AddItemToCartTest.java
+++ src/test/java/AddItemToCartTest.java
@@ -1,12 +1,19 @@
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pages.MyStorePages;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @Feature("Добавление товара в корзину")
 @Owner("Sarrriva")
@@ -16,15 +23,41 @@ public class AddItemToCartTest {
     @BeforeEach
     public void before() {
         step("Открыть страницу http://automationpractice.com/index.php", () -> {
-            open("http://automationpractice.com/index.php");
+            open("http://automationpractice.com/index.php?id_product=2&controller=product&search_query=Blouse&results=1");
         });
     }
 
     @Test
     @Story("Добавление товара в корзину")
     public void AddToCart() {
-        step("Кликнуть на кнопку добавления корзины", () -> {
-            MyStorePages.myStorePage.addToCartButton().click();
+        step("Проверка добавления товара в корзину", () -> {
+
+            MyStorePages.myStorePage.addToCartButton()
+                    .click();
+            MyStorePages.myStorePage.layerCart()
+                    .shouldBe(Condition.visible);
         });
+    }
+
+    @Test
+    @Story("Проверка ссылок")
+    @ParameterizedTest
+    @MethodSource("sizes")
+    public void LinkSkuTest(@Param String size,
+                            @Param String value) {
+        step("Проверка ссылки СКУ", () -> {
+            open("http://automationpractice.com/index.php?id_product=7&controller=product#/color-yellow/size-" + size);
+            MyStorePages.myStorePage.sizeInput()
+                    .shouldBe(Condition.exactValue(value));
+        });
+    }
+
+
+    static Stream<Arguments> sizes() {
+        return Stream.of(
+                arguments("l", "3"),
+                arguments("m", "2"),
+                arguments("s", "1")
+        );
     }
 }
--- src/test/java/pages/MyStore.java
+++ src/test/java/pages/MyStore.java
@@ -6,10 +6,14 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class MyStore {
 
+    public SelenideElement layerCart() {
+        return $("#layer_cart").as("модалка корзины");
+    }
+
     public SelenideElement addToCartButton() {
-        return $(".ajax_add_to_cart_button").as("кнопка добавления в корзину");     }
+        return $("#add_to_cart button").as("кнопка добавления в корзину");
+    }
 
-
-
-
+    public SelenideElement sizeInput() { return $("#group_1").as("Поле выбора размера");
+    }
 }
